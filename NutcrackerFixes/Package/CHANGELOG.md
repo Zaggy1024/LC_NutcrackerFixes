8109aad2de7d7c317fa43ae040a6a96895bf4989
Zaggy1024 <Zaggy1024@gmail.com>
2024-01-08 02:56:54 -0600

    Bump to version 1.0.1

--- NutcrackerFixes/Package/CHANGELOG.md
+++ NutcrackerFixes/Package/CHANGELOG.md
@@ -1,3 +1,6 @@
+## Version 1.0.1
+- Disabled logging in the shotgun hitreg loop
+
 ## Version 1.0.0
 - Fixed stutters on nutcrackers shooting other monsters
 - Improved shotgun hit registration to avoid non-registering shots
--- NutcrackerFixes/Plugin.cs
+++ NutcrackerFixes/Plugin.cs
@@ -11,7 +11,7 @@ namespace NutcrackerFixes
     {
         private const string MOD_NAME = "NutcrackerFixes";
         private const string MOD_UNIQUE_NAME = "Zaggy1024." + MOD_NAME;
-        private const string MOD_VERSION = "1.0.0";
+        private const string MOD_VERSION = "1.0.1";
 
         public const bool DEBUG_TRANSPILERS = false;
 
